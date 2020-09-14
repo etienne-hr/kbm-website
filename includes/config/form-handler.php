56e8b690eb5da43a516f02b8c54e14565129b0cd
etienne-hr <etienne.haller@hetic.net>
2020-09-14 12:28:17 +0200

    recaptcha with composer

--- includes/config/form-handler.php
+++ includes/config/form-handler.php
@@ -1,28 +1,8 @@
 <?php
 
-// include '.\includes\recaptcha\autoload.php';
+
 if (isset($_POST['submit'])) {
-    //recaptcha
 
-
-    // if (isset($_POST['g-recaptcha-response'])) {
-    //     $recaptcha = new \ReCaptcha\ReCaptcha('6LeqBsoZAAAAAHyb6B2QeGHu601NBfSuqzeTAoNY');
-    //     $resp = $recaptcha->verify($_POST['g-recaptcha-response']);
-    //     if ($resp->isSuccess()) {
-    //         echo '<pre>';
-    //         var_dump('valide');
-    //         echo '</pre>';
-    //     } else {
-    //         $errors = $resp->getErrorCodes();
-    //         echo '<pre>';
-    //         var_dump('captcha invalide');
-    //         echo '</pre>';
-    //     }
-    // } else {
-    //     echo '<pre>';
-    //     var_dump('nn rempli');
-    //     echo '</pre>';
-    // }
     $name = $_POST['name'];
     $lastname = $_POST['lastname'];
     $mailFrom = $_POST['mail'];
